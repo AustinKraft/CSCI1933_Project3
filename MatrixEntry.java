ea331fac93ab343d13a00cd6b8ffe70514ffc15f
ManaswineeB <bezba004@umn.edu>
2017-11-04 16:29:30 -0500

    Revert "setnext methods"
    This reverts commit 5690ff91d273540adf5391b379709a8cbfad47f0.

--- MatrixEntry.java
+++ MatrixEntry.java
@@ -4,8 +4,6 @@ public class MatrixEntry {
     private int data;
     private MatrixEntry nextRow;
     private MatrixEntry nextColumn;
-
-
     public MatrixEntry(int row, int col, int data){
         this.row = row;
         this.col = col;
@@ -23,7 +21,7 @@ public class MatrixEntry {
     public int getRow(){
         return this.row;
     }
-    public void setRow(int row){
+    public void setRow(int col){
         this.row = row;
     }
     public int getData(){
@@ -33,8 +31,14 @@ public class MatrixEntry {
         this.data = data;
     }
     public void setNextColumn(MatrixEntry el){
+<<<<<<< HEAD
         nextColumn = el;
 
+=======
+        nextColumn.setRow(el.getRow());
+        nextColumn.setColumn(el.getColumn());
+        nextColumn.setData(el.getData());
+>>>>>>> parent of 5690ff9... setnext methods
     }
     public MatrixEntry getNextRow(){
         return nextRow;
@@ -43,7 +47,13 @@ public class MatrixEntry {
         return nextColumn;
     }
     public void setNextRow(MatrixEntry el){
+<<<<<<< HEAD
         nextRow = el;
+=======
+        nextRow.setRow(el.getRow());
+        nextRow.setColumn(el.getColumn());
+        nextRow.setData(el.getData());
+>>>>>>> parent of 5690ff9... setnext methods
     }
 
     public static void main(String[] args) {
@@ -52,6 +62,7 @@ public class MatrixEntry {
         el.setNextColumn(el1);
         MatrixEntry el2 = new MatrixEntry(2,1,12);
         el.setNextRow(el2);
+
         System.out.println(el.getNextRow().getData());
         System.out.println(el.getNextColumn().getData());
     }
